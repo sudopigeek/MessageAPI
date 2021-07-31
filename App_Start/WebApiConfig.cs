a476ef0719bf009e48de78784c0533a2d9031b4e
BuildTools <unconfigured@null.spigotmc.org>
2021-07-31 13:52:09 -0700

    Add project files.

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace MessageAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                "DefaultApiWithId",
+                "{controller}/{id}",
+                new { id = RouteParameter.Optional },
+                new { id = @"\d+" });
+            config.Routes.MapHttpRoute(
+                "DefaultApiGet",
+                "{controller}",
+                new { action = "Get" },
+                new { httpMethod = new HttpMethodConstraint(HttpMethod.Get) });
+            config.Routes.MapHttpRoute(
+                "DefaultApiPost",
+                "{controller}",
+                new { action = "Post" },
+                new { httpMethod = new HttpMethodConstraint(HttpMethod.Post) });
+            config.Routes.MapHttpRoute(
+                "DefaultApiPut",
+                "{controller}",
+                new { action = "Put" },
+                new { httpMethod = new HttpMethodConstraint(HttpMethod.Put) });
+        }
+    }
+}
---
+++ Controllers/MainController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace MessageAPI.Controllers
+{
+    public class MainController : ApiController
+    {
+        public string[] Get()
+        {
+            List<string> lst = new List<string>();
+            foreach (Request req in messages)
+            {
+                lst.Add(req.Message);
+            }
+            return lst.ToArray();
+        }
+        public string Get(int id)
+        {
+            if (id < messages.Count && id > -1)
+            {
+                return messages[id].Message;
+            }
+            else
+                return "Invalid Id.";
+        }
+        public Response Post([FromBody] Request request)
+        {
+            messages.Add(request);
+            return new Response() { count = Count(), success = true };
+        }
+        public string Put(int id, [FromBody] string value)
+        {
+            if (id < messages.Count && id > -1)
+            {
+                messages[id].Message = value;
+                return "Successfully updated Id " + id + "'s message to '" + value + "'.";
+            }
+            else
+                return "Invalid Id.";
+        }
+        public string Delete(int id)
+        {
+            if (id < messages.Count && id > -1)
+            {
+                messages.RemoveAt(id);
+                return "Success.";
+            }
+            else
+                return "Invalid Id.";
+        }
+        private int Count()
+        {
+            int count = 0;
+            foreach (Request req in messages)
+            {
+                count += req.Message.Split(' ').Length;
+            }
+            return count;
+        }
+        private static List<Request> messages = new List<Request>()
+        {
+            new Request() { Id=0, Message="Message 1" },
+            new Request() { Id=1, Message="Message 2" }
+        };
+    }
+    public class Request
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+    }
+    public class Response
+    {
+        public int count { get; set; }
+        public bool success { get; set; }
+    }
+}
